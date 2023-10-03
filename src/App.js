ce5ffac4d9e2f7d0b9b3adb3e9d85c28a8094024
Gricelg517 <135929737+Gricelg517@users.noreply.github.com>
2023-10-02 19:38:26 -0500

    add buttom bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Gallery from './Components/Gallery';
+import Button from './Components/Button';
+
 
 function App() {
+  // Purpose: the id for the art in the met API
+  let [artId,setArtId] = useState(12720);
+
+  // The fetched data od saidart index
+  let [data,setData] = useState({});
+
+  // purpose:handle index iteration
+  const handleIterate =(e) => {
+  setArtId(artId + Number(e.target.value))
+  }
+
+// this will run when the app component is rendered
+useEffect(() => {
+    document.title='Welcome to Artworld'
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+    // makin the response into a json
+    .then(response => response.json())
+    // setting our data state variable to saud json data
+    .then(resData => setData(resData))
+    
+}, [artId]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Gallery objectImg={data.primaryImage} artist={data.artistDisplayName} title={data.title} />
+      <Button handleIterate ={handleIterate} />
+      </div>
   );
 }
 
